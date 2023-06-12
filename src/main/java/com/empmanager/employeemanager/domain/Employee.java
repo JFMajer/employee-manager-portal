2bddcdab2e0caadc6b390ae8afdd8bb5a85c8771
JFMajer <jakubmajer@live.com>
2023-06-12 18:26:23 +0200

    added EmployeeRepository

--- src/main/java/com/empmanager/employeemanager/domain/Employee.java
+++ src/main/java/com/empmanager/employeemanager/domain/Employee.java
@@ -10,10 +10,41 @@ public record Employee(
     Long id,
     @NotBlank(message = "Name is mandatory")
     String name,
+    @NotBlank(message = "Email is mandatory")
     String email,
+    @NotBlank(message = "Job title is mandatory")
     String jobTitle,
+    @NotBlank(message = "Phone is mandatory")
     String phone,
     String imageUrl,
-    String employeeCode
+    @NotBlank(message = "Employee code is mandatory")
+    String employeeCode,
+    @CreatedDate
+    Instant createdDate,
+    @LastModifiedDate
+    Instant lastModifiedDate
 ) {
+    public static Employee of(
+        Long id,
+        String name,
+        String email,
+        String jobTitle,
+        String phone,
+        String imageUrl,
+        String employeeCode,
+        Instant createdDate,
+        Instant lastModifiedDate
+    ) {
+        return new Employee(
+            id,
+            name,
+            email,
+            jobTitle,
+            phone,
+            imageUrl,
+            employeeCode,
+            createdDate,
+            lastModifiedDate
+        );
+    }
 }
