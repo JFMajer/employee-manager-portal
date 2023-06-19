7fa53d825a6d63ffb0e79ec7e3dd2341034bb36c
JFMajer <jakubmajer@live.com>
2023-06-19 19:53:25 +0200

    more rest paths

--- src/main/java/com/empmanager/employeemanager/controller/EmployeeResource.java
+++ src/main/java/com/empmanager/employeemanager/controller/EmployeeResource.java
@@ -1,8 +1,10 @@
 package com.empmanager.employeemanager.controller;
 
+import com.empmanager.employeemanager.domain.Employee;
 import com.empmanager.employeemanager.service.EmployeeService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employees")
@@ -11,6 +13,41 @@ public class EmployeeResource {
 
     public EmployeeResource(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    // return all employees
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<Employee>> getAllEmployees() {
+        Iterable<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    // return employee by id
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    // add employee
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    // update employee
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+    }
+
+    // delete employee
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
